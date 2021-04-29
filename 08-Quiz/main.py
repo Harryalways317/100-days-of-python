6f24de507240c40fbb2a6ba6b9b3097051fbffd6
Harish Vadaparty <harishvadaparty@gmail.com>
2021-04-30 02:02:53 +0530

    Update main.py

--- 08-Quiz/main.py
+++ 08-Quiz/main.py
@@ -3,11 +3,13 @@ from question_model import Question
 from quiz_brain import QuizBrain
 question_bank = []
 for q_data in question_data:
-    question_bank.append(Question(q_data["text"],q_data["answer"]))
+    question_bank.append(Question(q_data["question"],q_data["correct_answer"]))
 
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+print("Yay you have completed the quiz")
+print(f'Your final score is {quiz.user_score}/{quiz.question_number}')
 
 
