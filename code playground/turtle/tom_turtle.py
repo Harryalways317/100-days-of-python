dfb296e990dab16a5c85593c42308c5b00af2a58
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-01 01:10:12 +0530

    Update tom_turtle.py

--- code playground/turtle/tom_turtle.py
+++ code playground/turtle/tom_turtle.py
@@ -4,9 +4,16 @@ tom_the_turtle = Turtle()
 tom_the_turtle.shape("turtle")
 tom_the_turtle.color("red")
 tom_the_turtle.pencolor("blue")
-for i in range(0,4):
-    tom_the_turtle.forward(100)
-    tom_the_turtle.right(90)
+def square():
+    for i in range(0,4):
+        tom_the_turtle.forward(100)
+        tom_the_turtle.right(90)
+def dashed_line(length):
+    for i in range(0,steps=5,length=10):
+        tom_the_turtle.forward(steps)
+        tom_the_turtle.pendown()
+        tom_the_turtle.forward(steps)
+        tom_the_turtle.penup()
 
 screen = Screen()
 screen.exitonclick()
