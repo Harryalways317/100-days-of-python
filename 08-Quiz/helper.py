2a015616a327e18b3bfca835ccbb1997c356df65
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-02 23:17:51 +0530

    Update helper.py

--- 08-Quiz/helper.py
+++ 08-Quiz/helper.py
@@ -1,12 +1,8 @@
 #API from https://opentdb.com/api_config.php
 #TODO: implement multiple user choice based questions
 import requests
-no_of_questions = input("Enter no of questions ")
-difficulty = "easy"
-key = f"https://opentdb.com/api.php?amount={no_of_questions}&category=18&difficulty={difficulty}&type=boolean"
+key = f"https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean"
 response = requests.get(key)
 json_response = response.json()
 
 question_data = json_response["results"]
-
-print(question_data)
