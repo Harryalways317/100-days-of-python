bae1b6f4e972e754ebaaf8a3d767afef7e4e7a63
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-02 23:42:42 +0530

    Create main.py

---
+++ 09-Hirist Dot Painting/main.py
@@ -0,0 +1,8 @@
+import turtle
+# import colorgram
+
+# colors = colorgram.extract("09-Hirist Dot Painting/1.jpg", 10)
+# rgb_colors = [i.rgb for i in colors]
+# rgb_tuple = [tuple(i) for i in rgb_colors]
+# print(rgb_tuple)
+color_list = [(252, 251, 247), (188, 74, 20), (56, 34, 13), (149, 26, 9), (237, 226, 77), (24, 31, 60), (113, 167, 210), (45, 85, 143), (227, 243, 238)]
