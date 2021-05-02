101454aca9c23ac1511a49fc128c706a2f22ec57
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-03 00:53:12 +0530

    Update main.py

--- 09-Hirist Dot Painting/main.py
+++ 09-Hirist Dot Painting/main.py
@@ -5,4 +5,9 @@ import turtle
 # rgb_colors = [i.rgb for i in colors]
 # rgb_tuple = [tuple(i) for i in rgb_colors]
 # print(rgb_tuple)
+
+#10x10 rows of spots
+# spots around 20 in size
+#space around 50 in size
+
 color_list = [(252, 251, 247), (188, 74, 20), (56, 34, 13), (149, 26, 9), (237, 226, 77), (24, 31, 60), (113, 167, 210), (45, 85, 143), (227, 243, 238)]
