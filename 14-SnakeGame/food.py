4fc22772614417d8db68ebe80ed4ac7e587f76f1
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-07 00:52:23 +0530

    Update food.py

--- 14-SnakeGame/food.py
+++ 14-SnakeGame/food.py
@@ -3,13 +3,14 @@ import random
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circular")
+        self.shape("circle")
         self.penup()
-        self.shapesize(strech_len = 0.5,strech_wid = 0.5)
+        self.shapesize(stretch_len=0.5,stretch_wid = 0.5)
         self.color("blue")
         self.speed("fastest")
+        
+        
+    def refresh(self):
         rand_x = random.randint(-270, 270)
         rand_y = random.randint(-270, 270)
         self.goto(rand_x,rand_y)
-        
-        food.goto(random.randint(0, 200),random.randint(0, 200))
