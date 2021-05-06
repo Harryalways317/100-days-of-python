b6b3e40e9ec8f7bbcaca00cf18344467b82956ef
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-06 22:35:03 +0530

    Update main.py

---
+++ 13-TurtleGraphicFlakes/main.py
@@ -0,0 +1,77 @@
+from turtle import *
+
+
+speed('fastest')
+
+# turning the turtle to face upwards
+rt(-90)
+
+# the acute angle between
+# the base and branch of the Y
+angle = 30
+# function to plot a Y
+def y(sz, level,angle):
+
+	if level > 0:
+		colormode(255)
+		
+		# splitting the rgb range for green
+		# into equal intervals for each level
+		# setting the colour according
+		# to the current level
+		pencolor(0, 255//level, 0)
+		fd(sz)
+		rt(angle)
+        
+		y(0.8 * sz, level-1)
+		
+		pencolor(0, 255//level, 0)
+		
+		lt( 2 * angle )
+
+		# recursive call for
+		# the left subtree
+		y(0.8 * sz, level-1)
+		
+		pencolor(0, 255//level, 0)
+		
+		rt(angle)
+		fd(-sz)
+		
+		
+# tree of size 80 and level 7
+y(80, 7)
+
+def z(sz, level):
+
+	if level > 0:
+		colormode(255)
+		
+		# splitting the rgb range for green
+		# into equal intervals for each level
+		# setting the colour according
+		# to the current level
+		pencolor(0, 255//level, 0)
+		
+		# drawing the base
+		fd(sz)
+
+		rt(angle1)
+
+		# recursive call for
+		# the right subtree
+		(0.8 * sz, level-1)
+		
+		pencolor(0, 255//level, 0)
+		
+		lt( 2 * angle1 )
+
+		# recursive call for
+		# the left subtree
+		y(0.8 * sz, level-1)
+		
+		pencolor(0, 255//level, 0)
+		
+		rt(angle1)
+		fd(-sz)
+z(80, 7)
