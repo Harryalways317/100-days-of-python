860a175a5ef244c2289eab1731f8a1c0d6dd7f2c
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-14 16:56:07 +0530

    Update main.py

--- 14-SnakeGame/main.py
+++ 14-SnakeGame/main.py
@@ -15,7 +15,7 @@ food = Food()
 scoreboard = ScoreBoard()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
+screen.onkey(snake.up , "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
