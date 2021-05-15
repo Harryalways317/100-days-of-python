efe7c7d3d655c980f713448db2281d080ba34f50
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-15 23:41:42 +0530

    Update main.py

--- 16-TurtleCrossing/main.py
+++ 16-TurtleCrossing/main.py
@@ -3,13 +3,15 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Crossy Roads")
 screen.tracer(0)
 player = Player()
 car_manager = CarManager()
+scoreboard = ScoreBoard()
 screen.listen()
 screen.onkey(player.up,"w")
 
@@ -19,7 +21,19 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_car()
+    
 
 
     #player collison with car
+    for car in car_manager.all_cars:
+        if(car.distance(player) < 20):
+            game_is_on = False
+            scoreboard.game_over()
+
+    #sucessful crossing
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.increase_score()
     
+screen.exitonclick()
