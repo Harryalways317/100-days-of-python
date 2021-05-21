b715ee7bd6c5f62ced306d5146c61143f55c52d3
Harish Vadaparty <harishvadaparty@gmail.com>
2021-05-21 23:48:09 +0530

    Update main.py

--- 18-SquirrelCencus/main.py
+++ 18-SquirrelCencus/main.py
@@ -1,5 +1,16 @@
+
 import pandas as pd
 
 squirrel_data = pd.read_csv("Squirrel_Data.csv")
-print(squirrel_data)
-color = squirrel_data['Primary Fur Color']
+grey_color = len(squirrel_data[squirrel_data['Primary Fur Color'] == "Gray"])
+cinnamon_color = len(squirrel_data[squirrel_data['Primary Fur Color'] == "Cinnamon"])
+black_color = len(squirrel_data[squirrel_data['Primary Fur Color'] == "Black"])
+print(grey_color)
+print(cinnamon_color)
+print(black_color)
+data_dict = {
+  "Fur color" : ["Gray","Cinnamon","Black"],
+  "count" : [grey_color,cinnamon_color,black_color]
+}
+data = pd.DataFrame(data_dict)
+data.to_csv("Squirrel_color.csv")
